60861bc85bf9c7bbfcf5c880a55c35b35067f553
Anisa <anisahamohamed@gmail.com>
2020-01-11 20:54:37 +0000

    🎨 :art: Add schema to database and :fire: :fire: delete widgets and users tables from database

--- db/schema/04_submission_responses.sql
+++ db/schema/04_submission_responses.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS submission_responses CASCADE;
 
 CREATE TABLE submission_responses(
-  poll_option_id VARCHAR(255) NOT NULL REFERENCES poll_options(id) ON DELETE CASCADE,
-  submission_id VARCHAR(255) NOT NULL REFERENCES submissions(id) ON DELETE CASCADE,
+  poll_option_id INTEGER NOT NULL REFERENCES poll_options(id) ON DELETE CASCADE,
+  submission_id INTEGER NOT NULL REFERENCES submissions(id) ON DELETE CASCADE,
   submission_response BOOLEAN
 );
