9f7a29b8bd3f9999f1763257c623988bd9411925
Anisa <anisahamohamed@gmail.com>
2020-01-12 00:11:11 +0000

    :art added dummy data to polls and poll_options

--- db/schema/03_poll_options.sql
+++ db/schema/03_poll_options.sql
@@ -4,6 +4,6 @@ CREATE TABLE poll_options  (
   id SERIAL PRIMARY KEY NOT NULL,
   poll_id VARCHAR(255) NOT NULL REFERENCES polls(id) ON DELETE CASCADE,
   submitter_name VARCHAR(255),
-  date_option TIMESTAMP,
-  time_option TIMESTAMP
+  date_option DATE,
+  time_option TIME
 );
